c782032758640f1e84bc12091bc130aed913c723
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2025-01-20 17:57:30 +0000

    Render all projects page via JavaScript

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import "./styles.css";
 import { project } from "./project.js";
 import { task } from "./task.js";
 
-let allProject = [];
+let allProjects = [];
 
-let project1 = new project("To Do List Application", "A programming project within The Odin Project's JavaScript Course.");
+let project1 = new project("Default Project", "This is the default project present when the user first runs the app.");
 let task1 = new task("Initial Setup", "Initialize Webpack and create the required starting files", "01-17-2025", 1);
 let task2 = new task("Create Project Class", "Create project.js and a project class", "01-17-2025", 1);
 
 project1.addTask(task1);
 project1.addTask(task2);
-allProject.push(project1);
+allProjects.push(project1);
 
 console.log(project1);
 
-
+project.displayAllProjects(allProjects);
--- src/project.js
+++ src/project.js
@@ -1,3 +1,5 @@
+import addImage from "./images/add.svg";
+
 class project {
     title;
     description;
@@ -10,6 +12,53 @@ class project {
 
     addTask(task) {
         this.tasks.push(task);
+    }
+
+    displayProject() {
+        
+    }
+
+    static displayAllProjects(projectsArray) {
+        let subTitle = document.querySelector("#sub-title");
+        subTitle.textContent = "All Projects";
+
+        let content = document.querySelector(".content-container");
+
+        projectsArray.forEach(project => {
+            let projectDiv = document.createElement("div");
+            projectDiv.setAttribute("class", "project");
+
+            let textDiv = document.createElement("div");
+            textDiv.setAttribute("class", "project-text");
+
+            let projectTitle = document.createElement("div");
+            projectTitle.setAttribute("class", "project-title");
+            projectTitle.textContent = project.title;
+
+            let projectDesc = document.createElement("div");
+            projectDesc.setAttribute("class", "project-description");
+            projectDesc.textContent = project.description;
+
+            let projectButton = document.createElement("button");
+            projectButton.setAttribute("class", "project-button");
+            projectButton.textContent = "Open";
+
+            textDiv.appendChild(projectTitle);
+            textDiv.appendChild(projectDesc);
+            projectDiv.appendChild(textDiv);
+            projectDiv.appendChild(projectButton);
+            content.appendChild(projectDiv);
+        });
+
+        let newProject = document.createElement("div");
+        newProject.setAttribute("class", "project new-project");
+        let newImg = document.createElement("img");
+        newImg.setAttribute("id", "add");
+        newImg.src = addImage;
+        newImg.alt = "New Project";
+
+        newProject.appendChild(newImg);
+        content.appendChild(newProject);
     }
 }
 
